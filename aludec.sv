e55696f55d32808cb2aeee172528d717a4460e2b
abdelrahman ragab hashem <ar1813@fayoum.edu.eg>
2019-10-01 19:09:39 +0200

    aludec remove wrong i-type

--- aludec.sv
+++ aludec.sv
@@ -7,13 +7,13 @@ always_comb
         3'b000: alucontrol <= 4'b0010; // add (for lw/sw/addi)
         3'b001: alucontrol <= 4'b0110; // sub (for beq)
         3'b011: alucontrol <= 4'b0001; // or (for ori)
-        3'b100: alucontrol <= 4'b1000; // << (for srl)
         default: case(funct) // R-type instructions
             6'b100000: alucontrol <= 4'b0010; // add
             6'b100010: alucontrol <= 4'b0110; // sub
             6'b100100: alucontrol <= 4'b0000; // and
             6'b100101: alucontrol <= 4'b0001; // or
             6'b101010: alucontrol <= 4'b0111; // slt
+            6'b000010: alucontrol <= 4'b1000; // srl
             default: alucontrol <= 3'bxxx; // ???
         endcase
     endcase
