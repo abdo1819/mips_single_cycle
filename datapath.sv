9817f8863644a0154abb77e9c22e17937a206283
abdelrahman ragab hashem <ar1813@fayoum.edu.eg>
2019-10-02 14:54:12 +0200

    add half and byte signal for lh and lb

--- datapath.sv
+++ datapath.sv
@@ -3,7 +3,7 @@ module datapath(input logic clk, reset,
                 input logic [1:0] alusrc,
                 input logic ne,
                 input logic regdst,
-                input logic regwrite, jump,
+                input logic regwrite, jump, half,b,
                 input logic [3:0] alucontrol,
                 output logic zero,
                 output logic [31:0] pc,
@@ -16,7 +16,12 @@ module datapath(input logic clk, reset,
     logic [31:0] pcnext, pcnextbr, pcplus4, pcbranch;
     logic [31:0] signimm, signimmsh;
     logic [31:0] srca, srcb;
-    logic [31:0] result;
+    logic [31:0] result; // datamemory after the one byte design
+    logic [31:0] result_T; 
+    logic [31:0] half_result_extended;
+    logic [31:0] hw_dataMemeoryOutput; // datamemory after the half word design
+    logic [31:0] one_byte_result_sign_extended;
+
 
     // next PC logic
     flopr #(32) pcreg(clk, reset, pcnext, pc);
@@ -27,17 +32,28 @@ module datapath(input logic clk, reset,
 
     adder pcadd2(pcplus4, signimmsh, pcbranch); //branch or jumb
 
+    //half
+    signext se2(result_T[15:0], half_result_extended); //extend sign
+    //mux after the halfword
+    mux2 #(32) halfmux(result_T,half_result_extended,half,hw_dataMemeoryOutput);
+    // one byte
+    signex #(24,8) se3(result_T[7:0], one_byte_result_sign_extended);
+    //mux after the one byte word
+    mux2 #(32) ob_mux(hw_dataMemeoryOutput,
+                    one_byte_result_sign_extended,
+                    b,
+                    result);
+
     mux2 #(32) pcbrmux(pcplus4, pcbranch, pcsrc, pcnextbr);
     mux2 #(32) pcmux(pcnextbr, {pcplus4[31:28],
                     instr[25:0], 2'b00}, jump, pcnext);
 
-    // register file logic
     regfile rf(clk, regwrite, instr[25:21], instr[20:16],
                 writereg, result, srca, writedata);
 
     mux2 #(5) wrmux(instr[20:16], instr[15:11],
                     regdst, writereg);
-    mux2 #(32) resmux(aluout, readdata, memtoreg, result);
+    mux2 #(32) resmux(aluout, readdata, memtoreg, result_T);
 
     signext se(instr[15:0], signimm); //extend sign
 
--- maindec.sv
+++ maindec.sv
@@ -5,24 +5,29 @@ module maindec(input logic [5:0] op,
     output logic regdst, regwrite,
     output logic jump,
     output logic ne,
+    output logic half,
+    output logic b,
     output logic [2:0] aluop);
 
-logic [11:0] controls;
+logic [13:0] controls;
 
 assign {regwrite, regdst, alusrc, branch, memwrite,
-        memtoreg, jump, aluop, ne} = controls;
+        memtoreg, jump, aluop, ne, half, b} = controls;
 
 
 always_comb
     case(op)
-        6'b000000: controls <= 12'b110000000100; // RTYPE
-        6'b100011: controls <= 12'b100100100000; // LW
-        6'b101011: controls <= 12'b000101000000; // SW
-        6'b000100: controls <= 12'b000010000010; // BEQ
-        6'b001000: controls <= 12'b100100000000; // ADDI
-        6'b001101: controls <= 12'b101100000110; // ORI
-        6'b000010: controls <= 12'b000000010000; // J
-        6'b000101: controls <= 12'b000010000011;  // BNQ
-        default: controls <= 9'bxxxxxxxxx; // illegal op
+        6'b000000: controls <= 14'b11000000010000; // RTYPE
+        6'b100011: controls <= 14'b10010010000000; // LW
+        6'b101011: controls <= 14'b00010100000000; // SW
+        6'b000100: controls <= 14'b00001000001000; // BEQ
+        6'b001000: controls <= 14'b10010000000000; // ADDI
+        6'b001101: controls <= 14'b10110000011000; // ORI
+        6'b000010: controls <= 14'b00000001000000; // J
+        6'b000101: controls <= 14'b00001000001100;  // BNQ
+        6'b100001: controls <= 14'b10010010000010; // LH
+        6'b100000: controls <= 14'b10010010000011; // LB
+        default:   controls <= 14'bxxxxxxxxxxxxxx; // illegal op
+
     endcase
 endmodule
