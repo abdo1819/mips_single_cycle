8bf0c360b0159472ac76b37822427bba27798f73
abdelrahman ragab hashem <ar1813@fayoum.edu.eg>
2019-10-07 15:59:24 +0200

    merge attemt #1 not working

--- maindec.sv
+++ maindec.sv
@@ -1,37 +1,48 @@
-module maindec(input logic [5:0] op,
+module maindec(input logic [5:0] op,funct,
     output logic memtoreg, memwrite,
     output logic branch, 
     output logic [1:0] alusrc,
     output logic regdst, regwrite,
-    output logic jump,
+    output logic jump,jr,
     output logic ne,
     output logic half,
     output logic b,
     output logic [2:0] aluop,
     output logic lbu, link);
 
-logic [15:0] controls;
+logic [16:0] controls;
 
 assign {regwrite, regdst, alusrc, branch, memwrite,
-        memtoreg, jump, aluop, ne, half, b, lbu, link} = controls;
+        memtoreg, jump,jr, aluop, ne, half, b, lbu, link} = controls;
 
 
 always_comb
+if((op == 0) && (funct==6'b001000)) begin
+       controls <= 17'b01000000100000000; // jr
+     end
+else
+begin
     case(op)
-        6'b000000: controls <= 16'b1100000001000000; // RTYPE
-        6'b100011: controls <= 16'b1001001000000000; // LW
-        6'b101011: controls <= 16'b0001010000000000; // SW
-        6'b000100: controls <= 16'b0000100000100000; // BEQ
-        6'b001000: controls <= 16'b1001000000000000; // ADDI
-        6'b001101: controls <= 16'b1011000001100000; // ORI
-        6'b000010: controls <= 16'b0000000100000000; // J
-        6'b000101: controls <= 16'b0000100000110000;  // BNQ
-        6'b100001: controls <= 16'b1001001000001000; // LH
-        6'b100000: controls <= 16'b1001001000001100; // LB
-        6'b100100: controls <= 16'b1001000000000010; // lbu
-        6'b001100: controls <= 16'b1011000010000000; //andi
-        6'b000011: controls <= 16'b1000000100000001; //jal
-        default:   controls <= 16'bxxxxxxxxxxxxxxxx; // illegal op
+        6'b000000: controls <= 17'b11000000001000000; // RTYPE
+        6'b100011: controls <= 17'b10010010000000000; // LW
+        6'b101011: controls <= 17'b00010100000000000; // SW
+        6'b000100: controls <= 17'b00001000000100000; // BEQ
+        6'b001000: controls <= 17'b10010000000000000; // ADDI
+        6'b001101: controls <= 17'b10110000001100000; // ORI
+        6'b000010: controls <= 17'b00000001000000000; // J        
+        6'b000101: controls <= 17'b00001000000110000;  // BNQ
+        6'b100001: controls <= 17'b10010010000001000; // LH
+        6'b100000: controls <= 17'b10010010000001100; // LB
+        6'b100100: controls <= 17'b10010000000000010; // lbu
+        6'b001100: controls <= 17'b10110000010000000; //andi
+        6'b000011: controls <= 17'b10000001000000001; //jal
+        6'b001111: controls <= 17'b10010000010000000; // LUI
+        6'b001110: controls <= 17'b10010000010100000; // XORI
+        6'b000110: controls <= 17'b00001000001000000; // Blez	     	
+        6'b001010: controls <= 17'b10010000011000000; //slti
+
+        default:   controls <= 17'bxxxxxxxxxxxxxxxxx; // illegal op
 
     endcase
+    end
 endmodule
--- mips.sv
+++ mips.sv
@@ -6,7 +6,7 @@ module mips(input logic clk, reset,
             input logic [31:0] readdata);
 
 logic memtoreg, ne, regdst,
-    regwrite, jump, pcsrc, zero, half, b, link;
+    regwrite, jump,jr, pcsrc, zero, half, b, link;
 
 logic [1:0] alusrc;
 
@@ -14,12 +14,12 @@ logic [3:0] alucontrol;
 
 controller c(instr[31:26], instr[5:0], zero,
             memtoreg, memwrite, pcsrc,
-            alusrc, regdst, regwrite, jump, ne, half, b,
-            alucontrol, jr,lbu, link);
+            alusrc, regdst, regwrite, jump,jr, ne, half, b,
+            alucontrol,lbu, link);
 
 datapath dp(clk, reset, memtoreg, pcsrc,
-            alusrc, ne, regdst, jr,lbu, link, regwrite, jump, half, b,
-            alucontrol, 
+            alusrc, ne, regdst, lbu, link, regwrite, jump,jr, half,b,
+            alucontrol,
             zero, pc, instr,
             aluout, writedata, readdata);
 
