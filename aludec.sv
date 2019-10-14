759e7260e9a1a78c03bcbff482f2779526a46634
abdelrahman ragab hashem <ar1813@fayoum.edu.eg>
2019-10-14 10:44:54 +0200

    Revert "division works and tested"
    This reverts commit 4901ae5fbc533d81b427d4e39f663ba1271019c4.

--- aludec.sv
+++ aludec.sv
@@ -1,23 +1,16 @@
 module aludec(input logic [5:0] funct,
-                input logic [3:0] aluop,
-                output logic [3:0] alucontrol,output logic spregwrite );
-// assign jr=funct==8;
+                input logic [2:0] aluop,
+                output logic [3:0] alucontrol,output logic jr,output logic spregwrite);
+assign jr=funct==8;
 assign spregwrite=funct==27;
-
 always_comb
-    
+	
     case(aluop)
-        4'b0000: alucontrol <= 4'b0010; // add (for lw/sw/addi)
-        4'b0001: alucontrol <= 4'b0110; // sub (for beq)
-        4'b0010: alucontrol <= 4'b1010; // blez        
-        4'b0011: alucontrol <= 4'b0001; // or (for ori)
-        4'b0100: alucontrol <= 4'b1000; // shift 16 (LUI)
-        4'b0101: alucontrol <= 4'b1001; // xor (for XORI)
-        4'b0110: alucontrol <= 4'b0111; // sub (for slti)
-        4'b0111: alucontrol <= 4'b0000; // and (for andi)
-            
+        3'b000: alucontrol <= 4'b0010; // add (for lw/sw/addi)
+        3'b001: alucontrol <= 4'b0110; // sub (for beq)
+        3'b011: alucontrol <= 4'b0001; // or (for ori)
+        3'b100: alucontrol <= 4'b0000; // and (for andi)
         default: case(funct) // R-type instructions
-            6'b000000: alucontrol <= 4'b0011; // sll
             6'b100000: alucontrol <= 4'b0010; // add
             6'b001001: alucontrol <= 4'b0010; // addiu
             6'b100010: alucontrol <= 4'b0110; // sub
