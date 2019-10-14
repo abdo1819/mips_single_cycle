ed559037a118b4b743c4d515e12b0ef9258dfd6c
abdelrahman ragab hashem <ar1813@fayoum.edu.eg>
2019-10-14 13:46:51 +0200

    fix: testbench file

--- testbench__lhlb.sv
+++ testbench__lhlb.sv
@@ -3,7 +3,7 @@ module testbench__lhlb();
 logic clk;
 logic reset;
 logic [31:0] writedata, dataadr;
-logic memwrite;
+logic [1:0 ]memwrite;
 
 // instantiate device to be tested
 top dut (clk, reset, writedata, dataadr, memwrite);
