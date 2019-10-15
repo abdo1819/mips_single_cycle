306375a3afe1fde57efae8d35bb6703454624c77
salehmahmoud98 <shmd398@gmail.com>
2019-10-16 01:59:39 +0200

    Update controller.sv

--- controller.sv
+++ controller.sv
@@ -9,13 +9,13 @@ module controller(input logic [5:0] op, funct,
                     output logic [1:0]half,
                     output logic b,
                     output logic [4:0] alucontrol,
-                    output logic lbu, link, spregwrite);
+                    output logic lbu, link, spregwrite, mf, resmove, spaddr);
 
 logic [3:0] aluop;
 logic branch;
 
 maindec md(op,funct, memtoreg, memwrite, branch,
-             alusrc, regdst, regwrite, jump,jr,ne, half, b, aluop, lbu, link, spregwrite);
+             alusrc, regdst, regwrite, jump,jr,ne, half, b, aluop, lbu, link, spregwrite, mf, resmove, spaddr);
 aludec ad(funct, aluop, alucontrol);
 
 logic  bne_rc;
